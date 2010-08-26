71e8494d538de43b7aa7fd864ab8870158952603
Justin Baker <bakermoto@gmail.com>
2010-08-26 07:16:10 -0400

    Added specs for example of validate_block

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,20 @@ rescue LoadError
 end
 
 $:.unshift(File.dirname(__FILE__) + '/../lib')
+require 'active_record'
+require 'active_record/version'
+require 'action_controller'
+require 'action_view'
+require 'action_mailer'
+
+
+ActiveRecord::Migration.verbose = false
+ActiveRecord::Base.establish_connection({:adapter => 'sqlite3', :database => ':memory:'})
+ActiveRecord::Schema.define do
+    create_table :people do |table|
+        table.column :hair, :string
+        table.column :hair_color, :string
+    end
+end
+
 require 'validate_block'
--- spec/validate_block_spec.rb
+++ spec/validate_block_spec.rb
@@ -1,5 +1,40 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
+class Person < ActiveRecord::Base 
+
+  HAIR_COLORS = %w{blond brown black}
+
+  def bald?
+    hair.nil?
+  end
+
+  validate_block :unless => :bald? do
+    presence_of  :hair
+    length_of    :hair, :within => 3..15
+    inclusion_of :hair_color, :in => HAIR_COLORS
+  end
+
+end
 describe "validate_block" do
-  # there wer currently no specs, I'm lazy and don't understand how to spec a module like this.
+
+  before(:each) do
+    @person = Person.new
+  end
+
+   it "returns true" do
+     @person.save.should == true #:nohair:
+   end
+
+   it "returns an error for long hair" do
+     @person.hair = "a lot of effin hair mannnnn"
+     @person.save.should == false
+   end
+
+   it "returns an error for red hair" do
+
+     @person.hair = "short"
+     @person.hair_color = "red"
+     @person.save.should == false
+   end
+
 end
